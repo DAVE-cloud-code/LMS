9163df0e83a51c7b804a823f7ce99567e3e643c9
DAVE-cloud-code <daviduche161@gmail.com>
2026-04-25 06:18:22 -0700

    added node mailer

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const sendEmail = require("../utils/email");
 
 exports.register = async (req, res) => {
   try {
